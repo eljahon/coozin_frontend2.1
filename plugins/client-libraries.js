e8429365f68ae25c8868248aaf3e24864ef94acb
BekzodRakhmonjonov <bekzod03233@mail.ru>
2022-11-24 09:52:51 +0500

    init: created project

--- plugins/client-libraries.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-import VueSlideoutPanel from 'vue2-slideout-panel'
-Vue.use(VueSlideoutPanel)
--- plugins/swaper.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css'
-Vue.component(Swiper)
-Vue.component(SwiperSlide)
-Vue.use(VueAwesomeSwiper)
