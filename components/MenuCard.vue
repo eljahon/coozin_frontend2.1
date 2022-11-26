72c889a4c12593e93d772a54e748d3cc1bedf06b
BekzodRakhmonjonov <bekzod03233@mail.ru>
2022-11-26 19:21:59 +0500

    feat: add new components

---
+++ components/MenuCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="group flex flex-col items-center
+    w-40 p-2 rounded-lg bg-gray-200 border
+    active:border-orange-600 hover:border-orange-600
+    hover:bg-white cursor-pointer"
+  >
+    <span
+      class="font-semibold leading-5 text-xl text-gray-600 group-active:text-orange-600 group-hover:text-orange-600"
+    >
+      {{ date }}
+    </span>
+    <span
+      class="font-semibold leading-5 text-sm text-gray-600 group-active:text-orange-600 group-hover:text-orange-600"
+    >
+      {{ month }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['date', 'month']
+}
+</script>
+
+<style scoped></style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <!--  Banner  -->
     <div class="container mx-auto py-8">
-      <div class="flex gap-4">
+      <div class="flex justify-center gap-4">
         <banner-card
           src="img-1"
           add-style="card-1"
@@ -40,11 +41,99 @@
         </div>
       </div>
     </div>
+
+    <!--  Menu section  -->
+    <div class="container mx-auto">
+      <div class="flex items-center justify-between mb-5">
+        <h2 id="menu" class="text-3xl font-semibold text-gray-800">
+          <a href="#menu">Меню</a>
+        </h2>
+        <div class="flex items-center gap-1">
+          <span class="font-normal text-orange-600 cursor-pointer">Посмотреть больше</span>
+          <the-icon src="right-arrow" />
+        </div>
+      </div>
+    </div>
+    <div class="container mx-auto overflow-x-scroll scroll-style">
+      <div class="flex items-center gap-3.5">
+        <div v-for="item in menuCard">
+          <menu-card
+            :date="item.date"
+            :month="item.month"
+          />
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        menuCard: [
+          {
+            date: 26,
+            month: "Сен"
+          },
+          {
+            date: 27,
+            month: "Сен"
+          },
+          {
+            date: 28,
+            month: "Сен"
+          },
+          {
+            date: 29,
+            month: "Сен"
+          },
+          {
+            date: 30,
+            month: "Сен"
+          },
+          {
+            date: 31,
+            month: "Сен"
+          },
+          {
+            date: 1,
+            month: "Окт"
+          },
+          {
+            date: 2,
+            month: "Окт"
+          },
+          {
+            date: 3,
+            month: "Окт"
+          },
+          {
+            date: 4,
+            month: "Окт"
+          },
+
+        ]
+      }
+    }
+  }
+</script>
+
 
 <style scoped>
+  .scroll-style::-webkit-scrollbar {
+    height: 6px;
+    border-radius: 24px;
+  }
+  .scroll-style::-webkit-scrollbar-track {
+    background: #f1f1f1;
+  }
+  .scroll-style::-webkit-scrollbar-thumb {
+    background: #888;
+  }
+  .scroll-style::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
   .card-1 {
     width: 312px;
     height: 330px;
