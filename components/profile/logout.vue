02e98ccb180d7ebec5addb5432d5c80699c20772
BekzodRakhmonjonov <bekzod03233@mail.ru>
2023-01-05 13:38:54 +0500

    feat: added some news

--- components/profile/logout.vue
+++ components/profile/logout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dm:p-6 p-3 bg-white rounded-2xl flex flex-col w-full sm:gap-6 gap-4">
-    <div @click="$router.back()" class="absolute top-0 left-0 -bottom-96 right-0 bg-gray-900 opacity-30"></div>
+    <div @click="$router.back()" class="absolute top-0 left-0 xl:-bottom-96 lg:-bottom-24 -bottom-14 right-0 bg-gray-900 opacity-30"></div>
     <div class="width absolute md:gap-9 gap-5 z-10 md:p-12 p-6 flex flex-col items-center justify-center bg-white position rounded-2xl">
       <the-icon src="logout-photo" />
       <p class="text-xl text-gray-600">Вы действительно хотите выйти?</p>
