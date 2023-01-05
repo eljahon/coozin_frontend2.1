470cffe98a041051f18558023f9f3bfb86375deb
BekzodRakhmonjonov <bekzod03233@mail.ru>
2023-01-05 13:31:31 +0500

    feat: added some news

--- components/profile/logout.vue
+++ components/profile/logout.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="p-6 bg-white rounded-2xl flex flex-col w-full gap-6">
+  <div class="dm:p-6 p-3 bg-white rounded-2xl flex flex-col w-full sm:gap-6 gap-4">
     <div @click="$router.back()" class="absolute top-0 left-0 -bottom-96 right-0 bg-gray-900 opacity-30"></div>
-    <div style="width: 548px; height: 392px" class="absolute gap-9 z-10	p-12 flex flex-col items-center justify-center bg-white position rounded-2xl">
+    <div class="width absolute md:gap-9 gap-5 z-10 md:p-12 p-6 flex flex-col items-center justify-center bg-white position rounded-2xl">
       <the-icon src="logout-photo" />
       <p class="text-xl text-gray-600">Вы действительно хотите выйти?</p>
-      <div class="flex items-center justify-end gap-5">
-        <button @click="$router.back()" class="text-orange-600 font-semibold p-3 rounded-3xl w-40 bg-white border border-orange-600 active:opacity-80 hover:opacity-80">Нет</button>
-        <button @click="logout" class="text-white font-semibold p-3 rounded-3xl w-40 bg-orange-600 active:opacity-80 hover:opacity-80">Да</button>
+      <div class="flex items-center justify-end sm:gap-5 gap-2">
+        <button @click="$router.back()" class="text-orange-600 font-semibold p-3 rounded-3xl sm:w-40 w-36 bg-white border border-orange-600 active:opacity-80 hover:opacity-80">Нет</button>
+        <button @click="logout" class="text-white font-semibold p-3 rounded-3xl sm:w-40 w-36 bg-orange-600 active:opacity-80 hover:opacity-80">Да</button>
       </div>
     </div>
   </div>
@@ -28,5 +28,22 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+.width {
+  width: 548px;
+  height: 392px;
+}
+@media screen and (max-width: 768px) {
+  .width {
+    width: 80%;
+    height: 320px;
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .width {
+    width: 90%;
+    height: 320px;
+  }
 }
 </style>
