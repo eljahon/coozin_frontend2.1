4cf6be97e1ba0c9bbc71ac496332143d30ab0114
Bekzod <Bekzod@Sharifjons-MacBook-Air.local>
2023-02-15 17:42:02 +0500

    feat: add skeleton loader

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,44 @@
 <template>
   <div>
-    <Loader :active="pending" />
-    <div class="main-styles pb-8">
+    <!-- <Loader :active="pending" /> -->
+    <div v-if="pending">
+      <div class="main-styles py-4">
+        <div class="container mx-auto xl:px-0 px-4 lg:flex hidden scroll-style overflow-x-scroll gap-4">
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+        <div class="container mx-auto py-6 xl:px-0 px-4 lg:flex hidden scroll-style overflow-x-scroll gap-4">
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+        <div class="container mx-auto xl:px-0 px-4 lg:flex hidden scroll-style overflow-x-scroll gap-4">
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+          <div role="status" class="flex items-center justify-center h-56 w-96 bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="main-styles pb-8">
       <!--  Banner  -->
       <div class="container mx-auto py-8 xl:px-0 px-4 lg:block hidden">
         <div class="lg:flex lg:justify-center lg:gap-4">
